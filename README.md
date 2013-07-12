f9dc7b18e223fc502f6fcd59d2f3fb225d339db3
Michael Dellanoce <michael.dellanoce@gmail.com>
2013-07-11 22:49:09 -0400

    handle token expiration and revokation

--- README.md
+++ README.md
@@ -2,8 +2,6 @@ Example demonstrating how to use Google's OAuth with PhoneGap
 
 # Running the example
 
-1. Create an application at [Google's API console](https://code.google.com/apis/console), and create an **installed application** client ID.
-1. Copy your client ID, client secret, and set your desired authorization scopes in www/js/index.js
 1. Clone this repo, and run ```npm install```
 1. Add a platform, for example ios ```cordova platform add ios```
 1. Build the platform's project ```cordova build ios```
--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,14 @@
 var googleapi = {
+    setToken: function(data) {
+        //Cache the token
+        localStorage.access_token = data.access_token;
+        //Cache the refresh token, if there is one
+        localStorage.refresh_token = data.refresh_token || localStorage.refresh_token;
+        //Figure out when the token will expire by using the current
+        //time, plus the valid time (in seconds), minus a 1 minute buffer
+        var expiresAt = new Date().getTime() + parseInt(data.expires_in, 10) * 1000 - 60000;
+        localStorage.expires_at = expiresAt;
+    },
     authorize: function(options) {
         var deferred = $.Deferred();
 
@@ -41,6 +51,7 @@ var googleapi = {
                     redirect_uri: options.redirect_uri,
                     grant_type: 'authorization_code'
                 }).done(function(data) {
+                    googleapi.setToken(data);
                     deferred.resolve(data);
                 }).fail(function(response) {
                     deferred.reject(response.responseJSON);
@@ -54,23 +65,104 @@ var googleapi = {
         });
 
         return deferred.promise();
+    },
+    getToken: function(options) {
+        var deferred = $.Deferred();
+
+        if (new Date().getTime() < localStorage.expires_at) {
+            deferred.resolve({
+                access_token: localStorage.access_token
+            });
+        } else if (localStorage.refresh_token) {
+            $.post('https://accounts.google.com/o/oauth2/token', {
+                refresh_token: localStorage.refresh_token,
+                client_id: options.client_id,
+                client_secret: options.client_secret,
+                grant_type: 'refresh_token'
+            }).done(function(data) {
+                googleapi.setToken(data);
+                deferred.resolve(data);
+            }).fail(function(response) {
+                deferred.reject(response.responseJSON);
+            });
+        } else {
+            deferred.reject();
+        }
+
+        return deferred.promise();
+    },
+    userInfo: function(options) {
+        return $.getJSON('https://www.googleapis.com/oauth2/v1/userinfo', options);
+    }
+};
+
+var app = {
+    client_id: '14325430122.apps.googleusercontent.com',
+    client_secret: 'kjG4087WLUuvC4AIrQEV0lAe',
+    redirect_uri: 'http://localhost',
+    scope: 'https://www.googleapis.com/auth/userinfo.profile',
+
+    init: function() {
+        $('#login a').on('click', function() {
+            app.onLoginButtonClick();
+        });
+
+        //Check if we have a valid token
+        //cached or if we can get a new
+        //one using a refresh token.
+        googleapi.getToken({
+            client_id: this.client_id,
+            client_secret: this.client_secret
+        }).done(function() {
+            //Show the greet view if we get a valid token
+            app.showGreetView();
+        }).fail(function() {
+            //Show the login view if we have no valid token
+            app.showLoginView();
+        });
+    },
+    showLoginView: function() {
+        $('#login').show();
+        $('#greet').hide();
+    },
+    showGreetView: function() {
+        $('#login').hide();
+        $('#greet').show();
+
+        //Get the token, either from the cache
+        //or by using the refresh token.
+        googleapi.getToken({
+            client_id: this.client_id,
+            client_secret: this.client_secret
+        }).then(function(data) {
+            //Pass the token to the API call and return a new promise object
+            return googleapi.userInfo({ access_token: data.access_token });
+        }).done(function(user) {
+            //Display a greeting if the API call was successful
+            $('#greet h1').html('Hello ' + user.name + '!');
+        }).fail(function() {
+            //If getting the token fails, or the token has been
+            //revoked, show the login view.
+            app.showLoginView();
+        });
+    },
+    onLoginButtonClick: function() {
+        //Show the consent page
+        googleapi.authorize({
+            client_id: this.client_id,
+            client_secret: this.client_secret,
+            redirect_uri: this.redirect_uri,
+            scope: this.scope
+        }).done(function() {
+            //Show the greet view if access is granted
+            app.showGreetView();
+        }).fail(function(data) {
+            //Show an error message if access was denied
+            $('#login p').html(data.error);
+        });
     }
 };
 
 $(document).on('deviceready', function() {
-    var $loginButton = $('#login a');
-    var $loginStatus = $('#login p');
-
-    $loginButton.on('click', function() {
-        googleapi.authorize({
-            client_id: 'YOUR CLIENT ID HERE',
-            client_secret: 'YOUR CLIENT SECRET HERE',
-            redirect_uri: 'http://localhost',
-            scope: 'https://www.googleapis.com/auth/analytics.readonly'
-        }).done(function(data) {
-            $loginStatus.html('Access Token: ' + data.access_token);
-        }).fail(function(data) {
-            $loginStatus.html(data.error);
-        });
-    });
+    app.init();
 });
